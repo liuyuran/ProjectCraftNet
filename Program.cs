3a7c7355d2cac987d61691db824d609c4e70459e
Miata <liuyuran719829142@hotmail.com>
2024-02-17 21:43:52 +0800

    加入一些必要依赖

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CommandLine;
+using ProjectCraftNet.config;
 
-Console.WriteLine("Hello, World!");
+namespace ProjectCraftNet;
+
+public static class Program {
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class CommandLineOptions {
+        [Option('c', "config", Required = false, HelpText = "配置文件路径", Default = "/application/config/config.toml")]
+        public required string ConfigPath { get; set; }
+    }
+
+    public static int Main(string[] args) {
+        Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(o => {
+            ConfigUtil.Instance.Init(o.ConfigPath);
+        });
+        Console.WriteLine("test");
+        return -100;
+    }
+}
---
+++ config/ConfigUtil.cs
@@ -0,0 +1,14 @@
+﻿using Tomlyn;
+
+namespace ProjectCraftNet.config;
+
+public class ConfigUtil {
+    public static ConfigUtil Instance { get; } = new();
+
+    public void Init(string configFilePath) {
+        var fileContent = File.ReadAllText(configFilePath);
+        var model = Toml.ToModel(fileContent);
+        var global = (string)model["global"]!;
+        Console.WriteLine(global);
+    }
+}
