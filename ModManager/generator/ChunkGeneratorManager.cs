1d15a38b1c3bf10e3c31a3185111393e0d888843
刘宇然 <liuyuran719829142@hotmail.com>
2024-03-05 10:30:57 +0800

    理论上完成0号世界区块生成

--- ModManager/generator/ChunkGeneratorManager.cs
+++ ModManager/generator/ChunkGeneratorManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Numerics;
 using Microsoft.Extensions.Logging;
 using ModManager.logger;
+using static ModManager.localization.LocalizationManager;
+using static ModManager.ModManager;
 
 namespace ModManager.generator;
 
@@ -15,13 +17,42 @@ public class ChunkGeneratorManager
 
     private ChunkGeneratorManager()
     {
+        // 仅注册主世界的区块生成器
         _generators.Add(0, new DefaultChunkGenerator());
     }
     
+    /// <summary>
+    /// 注册区块生成器
+    /// </summary>
+    /// <param name="worldId">世界编码</param>
+    /// <param name="generator">生成器实例</param>
+    public static void AddChunkGenerator(ulong worldId, IChunkGenerator generator)
+    {
+        if (Instance._generators.TryAdd(worldId, generator)) return;
+        Logger.LogWarning("{}", Localize(ModId, "Chunk generator for world {0} already exists, override it", worldId));
+        Instance._generators[worldId] = generator;
+    }
+    
+    /// <summary>
+    /// 删除区块生成器
+    /// </summary>
+    /// <param name="worldId">世界编码</param>
+    public static void RemoveChunkGenerator(ulong worldId)
+    {
+        if (Instance._generators.Remove(worldId)) return;
+        Logger.LogWarning("{}", Localize(ModId, "Chunk generator for world {0} not found", worldId));
+    }
+    
+    /// <summary>
+    /// 生成特定世界特定区块的基准初始数据
+    /// </summary>
+    /// <param name="worldId">世界编码</param>
+    /// <param name="chunkPosition">区块坐标，以1为单位</param>
+    /// <returns>区块数据</returns>
     public static BlockData[] GenerateChunkBlockData(ulong worldId, Vector3 chunkPosition)
     {
         if (Instance._generators.TryGetValue(worldId, out var generator))
-            return generator.GenerateChunkBlockData(worldId, chunkPosition);
+            return generator.GenerateChunkBlockData( 64, chunkPosition);
         Logger.LogWarning("No chunk generator found for world {}", worldId);
         return Array.Empty<BlockData>();
     }
--- ModManager/generator/DefaultChunkGenerator.cs
+++ ModManager/generator/DefaultChunkGenerator.cs
@@ -2,10 +2,47 @@
 
 namespace ModManager.generator;
 
+/// <summary>
+/// 初始世界区块生成器
+/// </summary>
 public class DefaultChunkGenerator: IChunkGenerator
 {
-    public BlockData[] GenerateChunkBlockData(ulong worldId, Vector3 chunkPosition)
+    private readonly FastNoiseLite _noise = new();
+    private static int GetBlockIndex(int x, int y, int z, int size)
     {
-        throw new NotImplementedException();
+        if (x > size) x %= size;
+        if (y > size) y %= size;
+        if (z > size) z %= size;
+        return z * size * size + y * size + x;
+    }
+
+    private void GenerateMainWorld(ref BlockData[] blockData, int chunkSize, Vector3 chunkPosition)
+    {
+        for (var x = 0; x < chunkSize; x++)
+        {
+            for (var z = 0; z < chunkSize; z++)
+            {
+                var noiseData = _noise.GetNoise(chunkPosition.X * chunkSize + x, chunkPosition.Z * chunkSize + z);
+                noiseData = Math.Abs(noiseData);
+                var height = (uint) Math.Round(noiseData * chunkSize);
+                for (var y = 0; y < chunkSize; y++)
+                {
+                    blockData[GetBlockIndex(x, y, z, chunkSize)] = new BlockData
+                    {
+                        // TODO: 选择合适的方块ID
+                        BlockId = (ulong)(noiseData > height ? 1 : 0)
+                    };
+                }
+            }
+        }        
+    }
+    
+    public BlockData[] GenerateChunkBlockData(int chunkSize, Vector3 chunkPosition)
+    {
+        _noise.SetSeed(DateTime.Now.Millisecond);
+        _noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
+        var blockData = new BlockData[chunkSize * chunkSize * chunkSize];
+        GenerateMainWorld(ref blockData, chunkSize, chunkPosition);
+        return blockData;
     }
 }
