8f67fd01edcd24124cae0c4b593ff7db8841f38b
刘宇然 <liuyuran719829142@hotmail.com>
2024-04-19 15:30:57 +0800

    终于能用了

--- LandSimulator/MainWindow.axaml.cs
+++ LandSimulator/MainWindow.axaml.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.IO;
+using System.Numerics;
 using Avalonia.Controls;
+using CoreMod.blocks;
 using CoreMod.generator;
-using ModManager.game.generator;
+using ModManager.game.block;
 using ModManager.utils;
 using static System.Drawing.Imaging.ImageFormat;
 
@@ -12,14 +14,23 @@ namespace LandSimulator;
 [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 public partial class MainWindow : Window
 {
-    private IChunkGenerator _generator = new DefaultChunkGenerator();
+    private readonly DefaultChunkGenerator _generator = new();
     private const int ChunkSize = 64;
 
     public MainWindow()
     {
         InitializeComponent();
-        var drawingBitmap = new Bitmap(ChunkSize, ChunkSize);
-        GeneratePreview(drawingBitmap, new IntVector3(0, 0, 0));
+        BlockManager.RegisterBlock<Air>();
+        BlockManager.RegisterBlock<Dirt>();
+        const int size = 12;
+        var drawingBitmap = new Bitmap(ChunkSize * size, ChunkSize * size);
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                GeneratePreview(drawingBitmap, new IntVector3(i, 0, j), new Vector2(i, j));                
+            }
+        }
         // 创建一个内存流
         using var memoryStream = new MemoryStream();
         // 将System.Drawing.Bitmap保存到内存流中
@@ -37,23 +48,23 @@ public partial class MainWindow : Window
         return z * size * size + y * size + x;
     }
     
-    private void GeneratePreview(Bitmap bitmap, IntVector3 chunkPosition)
+    private void GeneratePreview(Bitmap bitmap, IntVector3 chunkPosition, Vector2 offset)
     {
         var data = _generator.GenerateChunkBlockData(ChunkSize, chunkPosition);
         for (var i = 0; i < ChunkSize; i++)
         {
             for (var j = 0; j < ChunkSize; j++)
             {
-                var height = 0;
+                var height = ChunkSize;
                 for (var k = 0; k < ChunkSize; k++)
                 {
-                    if (data[GetBlockIndex(i, j, k, ChunkSize)].BlockId <= 0) continue;
+                    if (data[GetBlockIndex(i, k, j, ChunkSize)].BlockId != BlockManager.GetBlockId<Air>()) continue;
                     height = k;
                     break;
                 }
 
                 var percent = (float)height / ChunkSize;
-                bitmap.SetPixel(i, j, Color.FromArgb(
+                bitmap.SetPixel((int)(i + offset.X * ChunkSize), (int)(j + offset.Y * ChunkSize), Color.FromArgb(
                     (int)(255 * percent),
                     (int)(255 * percent),
                     (int)(255 * percent)
